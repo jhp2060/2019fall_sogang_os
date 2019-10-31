064130905dc023abeaf6bdfd437882e7bd626b60
kr12ey <cse20150632@cspro9.sogang.ac.kr>
2019-10-31 10:39:19 -0700

    implement halt, exit, write syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
+#include "devices/input.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,15 +17,20 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+	void *esp = f->esp;
+	int syscall_num = *(int*)esp;
+	esp += sizeof(int*);
 	
-	int *syscall_num = *(f->esp);
-	f->esp += sizeof(int*);
-	
-	switch (*syscall_num) {
+	switch (syscall_num) {
 		case SYS_HALT:
+			sys_halt();
 			break;
 		case SYS_EXIT:
+			ASSERT(is_valid_access(esp));
+			int status = *(int*)esp;
+			esp += sizeof(int*);
+
+			sys_exit(status);
 			break;
 		case SYS_EXEC:
 			break;
@@ -32,15 +39,52 @@ syscall_handler (struct intr_frame *f UNUSED)
 		case SYS_READ:
 			break;
 		case SYS_WRITE:
+			ASSERT(is_valid_access(esp));
+			
+			int fd = *(int*)esp;
+			esp += sizeof(int*);
+
+			ASSERT(is_valid_access(f->esp));
+			const void *buffer = *(void**)esp;
+			esp += sizeof(void**);
+
+			ASSERT(is_valid_access(esp));
+			unsigned size = *(unsigned*)esp;
+			esp += sizeof(unsigned*);
+			
+			ASSERT(is_valid_access(buffer));
+			f->eax = sys_write(fd, buffer, size);
 			break;
 	}
-
-  thread_exit ();
+	//thread_exit();
 }
 
-bool is_valid_access(void* user_addr){
+bool is_valid_access(void *user_addr){
 	struct thread *t = thread_current(); 
 	if (pagedir_get_page(t->pagedir, user_addr) && is_user_vaddr(user_addr) && !is_kernel_vaddr(user_addr))
 		return 1;
 	else return 0;
+}
+
+void sys_halt(void){
+	shutdown_power_off();
+}
+
+void sys_exit(int status){
+	printf("%s: exit(%d)\n", thread_name(), status);
+	thread_exit();
+}
+
+int sys_write(int fd, const void *buffer, unsigned size){
+	int written = 0;
+	if (fd == 1){
+		int i;
+		intr_set_level(INTR_OFF);
+		for (i=0;i<size;i++){
+			input_putc(*(uint8_t*)(buffer+i));
+			written++;
+		}
+		intr_set_level(INTR_ON);
+	}
+	return written;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -5,6 +5,10 @@
 
 void syscall_init (void);
 
-bool is_valid_access(void* user_addr);
+bool is_valid_access(void *user_addr);
+
+void sys_halt (void);
+void sys_exit (int status);
+int sys_write (int fd, const void *buffer, unsigned size);
 
 #endif /* userprog/syscall.h */
