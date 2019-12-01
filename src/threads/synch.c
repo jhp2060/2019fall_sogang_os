c73ee661ab4df20b667e3f5871de1b4b19dc14ca
kr12ey <cse20150632@cspro9.sogang.ac.kr>
2019-11-30 20:52:23 -0800

    minor fix

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem)
+      list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
