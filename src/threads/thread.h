6b546b39fdae0f0c49e1d5dbe82415cd87aee5be
kr12ey <cse20150632@cspro9.sogang.ac.kr>
2019-11-30 21:18:09 -0800

    minor fix

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -23,7 +23,7 @@ enum thread_status
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
-//typedef int tid_t;
+typedef int tid_t;
 #define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
 
 /* Thread priorities. */
@@ -104,6 +104,7 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+#ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
 		struct list children;
